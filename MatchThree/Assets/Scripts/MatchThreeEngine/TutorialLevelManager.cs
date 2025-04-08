c3e9e2f55415fd711ebb96d580c7177f60baec9f
Roman <romadoliba@gmail.com>
2025-04-08 19:04:30 +0300

    Made some UI for tutorLevel

--- MatchThree/Assets/Scripts/MatchThreeEngine/TutorialLevelManager.cs
+++ MatchThree/Assets/Scripts/MatchThreeEngine/TutorialLevelManager.cs
@@ -50,5 +50,10 @@ namespace MatchThreeEngine
             }
                                        
         }
+
+        private void OnDisable()
+        {
+            DOTween.KillAll(true);
+        }
     }
 }
